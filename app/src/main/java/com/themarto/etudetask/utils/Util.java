3ae0d7e5f7474b8c908551850cdbccb82cf68089
Olariaga Martin <olariagamartin@gmail.com>
2021-02-25 23:37:41 -0300

    When month and year math with the current and the day is not "today", "yesterday" or "tomorrow" -> the date doesn't shows

--- app/src/main/java/com/themarto/etudetask/utils/Util.java
+++ app/src/main/java/com/themarto/etudetask/utils/Util.java
@@ -53,6 +53,10 @@ public class Util {
             else if(myTime.get(Calendar.DAY_OF_MONTH) == actual.get(Calendar.DAY_OF_MONTH) + 1){
                 strDate = context.getString(R.string.date_format_tomorrow);
             }
+            else {
+                SimpleDateFormat format = new SimpleDateFormat("EEE, d MMM", Locale.getDefault());
+                strDate = format.format(time);
+            }
         }
         else {
             SimpleDateFormat format = new SimpleDateFormat("EEE, d MMM", Locale.getDefault());
