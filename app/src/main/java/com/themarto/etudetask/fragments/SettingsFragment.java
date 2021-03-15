9f15d15ee25d5ab551f5033fd5e0e69e84b0f1eb
Olariaga Martin <olariagamartin@gmail.com>
2021-03-14 22:50:28 -0300

    Change theme dynamically

--- app/src/main/java/com/themarto/etudetask/fragments/SettingsFragment.java
+++ app/src/main/java/com/themarto/etudetask/fragments/SettingsFragment.java
@@ -1,15 +1,42 @@
 package com.themarto.etudetask.fragments;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import com.themarto.etudetask.R;
 
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.preference.PreferenceFragmentCompat;
 
-public class SettingsFragment extends PreferenceFragmentCompat {
+public class SettingsFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener {
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if (key.equals("theme")) {
+            String theme = getPreferenceManager().getSharedPreferences().getString(key, "");
+            if (theme.equals("light")) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+            }
+            if (theme.equals("dark")) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            }
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        getPreferenceManager().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        getPreferenceManager().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
     }
 }
