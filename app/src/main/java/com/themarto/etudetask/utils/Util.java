8f0907e5ced6786b352938ef49220165c9bffeea
Olariaga Martin <olariagamartin@gmail.com>
2021-05-21 02:00:22 -0300

    Bug: when the day is tomorrow but it is also the next month it does not return the day as "tomorrow" but instead it return in "EEE d MMM" format

--- app/src/main/java/com/themarto/etudetask/utils/Util.java
+++ app/src/main/java/com/themarto/etudetask/utils/Util.java
@@ -21,22 +21,22 @@ public class Util {
 
     public static final String SELECTED_SUBJECT_KEY = "SELECTED_SUBJECT";
 
-    public static int resolveColorAttr (int colorAttr, Context context) {
+    public static int resolveColorAttr(int colorAttr, Context context) {
         TypedValue resolvedAttr = resolveThemeAttr(colorAttr, context);
         // resourceId is used if it's a ColorStateList, and data if it's a color reference or a hex color
         int colorRes = (resolvedAttr.resourceId != 0) ? resolvedAttr.resourceId : resolvedAttr.data;
         return ContextCompat.getColor(context, colorRes);
     }
 
-    public static boolean isDayPassed (Date date) {
+    public static boolean isDayPassed(Date date) {
         Calendar today = Calendar.getInstance();
         Calendar day = Calendar.getInstance();
         day.setTime(date);
         return (today.get(Calendar.YEAR) >= day.get(Calendar.YEAR)
-            && today.get(Calendar.DAY_OF_YEAR) > day.get(Calendar.DAY_OF_YEAR));
+                && today.get(Calendar.DAY_OF_YEAR) > day.get(Calendar.DAY_OF_YEAR));
     }
 
-    private static TypedValue resolveThemeAttr (int attrRes, Context context) {
+    private static TypedValue resolveThemeAttr(int attrRes, Context context) {
         TypedValue typedValue = new TypedValue();
         context.getTheme().resolveAttribute(attrRes, typedValue, true);
         return typedValue;
@@ -44,10 +44,10 @@ public class Util {
 
     public static String getDateStr(Date date, Context context, boolean hasTime) {
         String strDate = "";
-        if (date != null){
-            if(hasTime) {
+        if (date != null) {
+            if (hasTime) {
                 strDate = Util.getDateString(date, context) + ", " + Util.getTimeString(date);
-            } else{
+            } else {
                 strDate = Util.getDateString(date, context);
             }
         }
@@ -56,6 +56,7 @@ public class Util {
 
     /**
      * Return the date in format "Day, Month"
+     *
      * @param time the date to get day and month
      * @return formatted string
      */
@@ -65,41 +66,57 @@ public class Util {
         String strDate = "";
         myTime.setTime(time);
 
-        if (myTime.get(Calendar.MONTH) == actual.get(Calendar.MONTH)
-                && myTime.get(Calendar.YEAR) == actual.get(Calendar.YEAR)) {
-            if(myTime.get(Calendar.DAY_OF_MONTH) == actual.get(Calendar.DAY_OF_MONTH)){
-                strDate = context.getString(R.string.date_format_today);
-            } else if (myTime.get(Calendar.DAY_OF_MONTH) == actual.get(Calendar.DAY_OF_MONTH) - 1){
-                strDate = context.getString(R.string.date_format_yesterday);
-            }
-            else if(myTime.get(Calendar.DAY_OF_MONTH) == actual.get(Calendar.DAY_OF_MONTH) + 1){
-                strDate = context.getString(R.string.date_format_tomorrow);
-            }
-            else {
-                SimpleDateFormat format = new SimpleDateFormat("EEE, d MMM", Locale.getDefault());
-                strDate = format.format(time);
-            }
-        }
-        else {
+        if (isToday(time)) {
+            strDate = context.getString(R.string.date_format_today);
+        } else if (isYesterday(time)) {
+            strDate = context.getString(R.string.date_format_yesterday);
+        } else if (isTomorrow(time)) {
+            strDate = context.getString(R.string.date_format_tomorrow);
+        } else {
             SimpleDateFormat format = new SimpleDateFormat("EEE, d MMM", Locale.getDefault());
             strDate = format.format(time);
         }
+
         return strDate;
+    }
+
+    private static boolean isToday(Date date) {
+        Calendar today = Calendar.getInstance();
+        Calendar day = Calendar.getInstance();
+        day.setTime(date);
+        return today.get(Calendar.DAY_OF_YEAR) == day.get(Calendar.DAY_OF_YEAR);
+    }
+
+    private static boolean isYesterday(Date date) {
+        Calendar yesterday = Calendar.getInstance();
+        yesterday.add(Calendar.DAY_OF_YEAR, -1);
+        Calendar day = Calendar.getInstance();
+        day.setTime(date);
+        return yesterday.get(Calendar.DAY_OF_YEAR) == day.get(Calendar.DAY_OF_YEAR);
+    }
+
+    private static boolean isTomorrow(Date date) {
+        Calendar tomorrow = Calendar.getInstance();
+        tomorrow.add(Calendar.DAY_OF_YEAR, 1);
+        Calendar day = Calendar.getInstance();
+        day.setTime(date);
+        return tomorrow.get(Calendar.DAY_OF_YEAR) == day.get(Calendar.DAY_OF_YEAR);
     }
 
     /**
      * Return time in format "5:35 PM"
+     *
      * @param time the day to get hour and minute
      * @return formatted time
      */
-    public static String getTimeString(Date time){
+    public static String getTimeString(Date time) {
         String srtTime;
         SimpleDateFormat format = new SimpleDateFormat("h:mm a", Locale.getDefault());
         srtTime = format.format(time);
-        return  srtTime;
+        return srtTime;
     }
 
-    public static Data saveNotificationData(String title, String details, String taskId){
+    public static Data saveNotificationData(String title, String details, String taskId) {
         return new Data.Builder()
                 .putString(WorkManagerAlarm.DATA_KEY_TITLE, title)
                 .putString(WorkManagerAlarm.DATA_KEY_DETAIL, details)
@@ -107,19 +124,19 @@ public class Util {
                 .build();
     }
 
-    public static List<Task> getDoneTasks(List<Task> tasks){
+    public static List<Task> getDoneTasks(List<Task> tasks) {
         List<Task> doneTasks = new ArrayList<Task>();
-        for(Task task : tasks) {
-            if(task.isDone())
+        for (Task task : tasks) {
+            if (task.isDone())
                 doneTasks.add(task);
         }
         return doneTasks;
     }
 
-    public static List<Task> getToDoTasks(List<Task> tasks){
+    public static List<Task> getToDoTasks(List<Task> tasks) {
         List<Task> toDoTasks = new ArrayList<Task>();
-        for(Task task : tasks) {
-            if(!task.isDone())
+        for (Task task : tasks) {
+            if (!task.isDone())
                 toDoTasks.add(task);
         }
         return toDoTasks;
